1e9f41c7cbc4908b3f25ed0e7c11c53fe9a16655
Austin King <a0k08pr@homeoffice.wal-mart.com>
2021-12-07 12:01:22 -0600

    first

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hellow");
+        System.out.println("hello");
         var m = new Movie();
     }
 }
